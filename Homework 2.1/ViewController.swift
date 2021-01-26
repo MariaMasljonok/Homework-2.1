f7178a2db5988c4fe54e9f632ecf9a8ce550f1b9
Maria Masljonok <maria@onoffapp.com>
2021-01-26 18:15:09 +0200

    Initial Commit

---
+++ Homework 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.1
+//
+//  Created by Maria Masljonok on 26.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
