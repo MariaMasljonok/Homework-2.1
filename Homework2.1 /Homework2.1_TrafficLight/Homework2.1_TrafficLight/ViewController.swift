379db4b2e1d55516f364310e00a2d49fba9deff4
Maria Masljonok <maria@onoffapp.com>
2021-01-28 11:03:45 +0200

    color changed

---
+++ Homework2.1 /Homework2.1_TrafficLight/Homework2.1_TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2.1_TrafficLight
+//
+//  Created by Maria Masljonok on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
